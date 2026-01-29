dd395dba5022c9702613045ab52c51cbbda24526
ns-develops <test.tt9061030@gmail.com>
2026-01-29 16:44:04 +0100

    added routes

--- frontend/Saas-system-project/src/App.jsx
+++ frontend/Saas-system-project/src/App.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+/* Auth Pages */
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+
+/* Admin Pages */
+import Dashboard from "./pages/Admin/Dashboard";
+import ManageTasks from "./pages/Admin/ManageTasks";
+import CreateTask from "./pages/Admin/CreateTask";
+import ManageUsers from "./pages/Admin/ManageUsers";
+
+/* Routes / Guards */
+import PrivateRoute from "./routes/PrivateRoute";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Router>
+      <Routes>
 
-export default App
+        {/* Public Routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+
+        {/* Admin Routes */}
+        <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/tasks" element={<ManageTasks />} />
+          <Route path="/admin/createtasks" element={<CreateTask />} />
+          <Route path="/admin/users" element={<ManageUsers />} />
+        </Route>
+
+        {/* User Routes */}
+        <Route element={<PrivateRoute allowedRoles={["user"]} />}>
+          <Route path="/user/dashboard" element={<Dashboard />} />
+        </Route>
+
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
