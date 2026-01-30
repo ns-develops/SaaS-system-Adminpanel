0c612a6bfc8a23dd5c64693ca225999963245062
ns-develops <test.tt9061030@gmail.com>
2026-01-30 20:20:15 +0100

    server running

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+//middleware to handle cors
+app.use(
+    cors({
+        origin: process.env.client_URL || "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-type", "Authorization"],
+    })
+);
+
+//middleware
+app.use(express.json());
+
+//routes
+
+//app.use("api/auth", authRoutes);
+//app.use("/api/users", userRoutes);
+//app.use("api/tasks", taskRoutes);
+//app.use("/api/reports", reportRoutes);
+
+//start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port: ${PORT} `));
