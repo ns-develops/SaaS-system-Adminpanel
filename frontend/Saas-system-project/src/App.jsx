0bd1f7adb8314fe747defaad1aa549fed12d5724
ns-develops <test.tt9061030@gmail.com>
2026-01-30 16:49:16 +0100

    small upd

--- frontend/Saas-system-project/src/App.jsx
+++ frontend/Saas-system-project/src/App.jsx
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Navigate,
 } from "react-router-dom";
 
 /* Auth Pages */
@@ -15,16 +16,21 @@ import ManageTasks from "./pages/Admin/ManageTasks";
 import CreateTask from "./pages/Admin/CreateTask";
 import ManageUsers from "./pages/Admin/ManageUsers";
 
-/* Routes / Guards */
-import PrivateRoute from "./routes/PrivateRoute";
+/* User Pages */
 import UserDashboard from "./pages/User/UserDashboard";
 import MyTask from "./pages/User/MyTask";
 import ViewTaskDetails from "./pages/User/ViewTaskDetails";
 
+/* Routes / Guards */
+import PrivateRoute from "./routes/PrivateRoute";
+
 const App = () => {
   return (
-    <Router>
+    <Router basename="/SaaS-system-Adminpanel">
       <Routes>
+
+        {/* Root */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
 
         {/* Public Routes */}
         <Route path="/login" element={<Login />} />
@@ -44,6 +50,9 @@ const App = () => {
           <Route path="/user/tasks" element={<MyTask />} />
           <Route path="/user/tasks-details/:id" element={<ViewTaskDetails />} />
         </Route>
+
+        {/* 404 */}
+        <Route path="*" element={<h1>404 – Sidan finns inte</h1>} />
 
       </Routes>
     </Router>
